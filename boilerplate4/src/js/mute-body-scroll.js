27320f9cc67bd08245df5c8cd6fa43b23afacce1
Akat <295140755@qq.com>
2020-07-15 17:54:24 +0800

    Update mute-body-scroll.js
    add annotation for mute-body

--- boilerplate4/src/js/mute-body-scroll.js
+++ boilerplate4/src/js/mute-body-scroll.js
@@ -1,16 +1,22 @@
-let nowTop = 0;
-const bodyEl = document.body;
+let nowTop = 0
+const bodyEl = document.body
 
+/**
+ * stop the body element to scroll 
+ * when a fixed poper is shown
+ * @param {boolean} mute
+ * @returns {void}
+ */
 export default mute => {
-    if (mute) {
-        nowTop = window.scrollY;
+  if (mute) {
+    nowTop = window.scrollY
 
-        bodyEl.style.position = 'fixed';
-        bodyEl.style.top = -nowTop + 'px';
-    } else {
-        bodyEl.style.position = '';
-        bodyEl.style.top = '';
+    bodyEl.style.position = 'fixed'
+    bodyEl.style.top = -nowTop + 'px'
+  } else {
+    bodyEl.style.position = ''
+    bodyEl.style.top = ''
 
-        window.scrollTo(0, nowTop);
-    }
-};
+    window.scrollTo(0, nowTop)
+  }
+}
